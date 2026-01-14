4f458222b8931893b9bd936ce5734d8923a8fd32
maxamedjaamac34 <shideoomaar10@gmail.com>
2026-01-13 21:33:19 -0500

    Remove dev-only allowedDevOrigins from Next config

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://10.10.240.23:3000",
-    ],
-  },
 };
 
 export default nextConfig;
