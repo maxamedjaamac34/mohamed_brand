290a51055e4b1f388ffa266cb9cb5c9425adf765
maxamedjaamac34 <shideoomaar10@gmail.com>
2026-01-14 18:15:03 -0500

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   devIndicators: false,
 };
